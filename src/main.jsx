e5516f9215db607e06c223cb4a50bdab96e212bd
arnaudsm <arnaud.desaintmeloir@gmail.com>
2024-01-21 23:50:08 -0500

    21 new comics

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const comicCount = 298;
+const comicCount = 320;
 
 function App() {
   const [number, setNumber] = useState(comicCount);
